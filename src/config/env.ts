ec6ea48e3110dbeb0f0e1a5b2465673803563b7f
Rahul Dutta <rahulstudy119@gmail.com>
2026-06-23 01:48:33 +0530

    refactor: replace manual env checks with zod validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,23 +1,13 @@
 import "dotenv/config"
+import z from "zod";
 
-if(!process.env.DATABASE_URL) {
-    throw new Error("DATABASE URL is missing");
-}
-if(!process.env.PORT) {
-    throw new Error("Port is missing");
-}
-if(!process.env.JWT_ACCESS_SECRET) {
-    throw new Error("JWT_ACCESS_SECRET is missing");
-}
-if(!process.env.JWT_REFRESH_SECRET) {
-    throw new Error("JWT_REFRESH_SECRET is missing");
-}
+const envSchema = z.object({ 
+    PORT: z.coerce.number().default(3000),
+    DATABASE_URL: z.url(),
+    JWT_ACCESS_SECRET: z.string().min(16),
+    JWT_REFRESH_SECRET: z.string().min(16)
+});
 
-const env = {
-    PORT: Number(process.env.PORT),
-    DATABASE_URL: process.env.DATABASE_URL!,
-    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
-    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!
-}
+const env = envSchema.parse(process.env)
 
 export default env
